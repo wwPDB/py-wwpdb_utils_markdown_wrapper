f91fcee949582731f430f99f4bb2be26e6873591
Ezra Peisach <ezra.peisach@rcsb.org>
2019-01-06 16:51:51 -0500

    Classifiers should be a list

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url='https://github.com/rcsb/py-wwpdb_utils_markdown_wrapper',
     #
     license='Apache 2.0',
-    classifiers=(
+    classifiers=[
         'Development Status :: 3 - Alpha',
         # 'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -39,10 +39,10 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ),
+    ],
     #
     # Code highlighting is better with pygments
-    install_requires=['graphviz', 'Markdown>=3.0.0', 'beautifulsoup4',
+    install_requires=['graphviz', 'Markdown~=3.0', 'beautifulsoup4',
                       'pygments'],
     packages=find_packages(exclude=['wwpdb.utils.tests-markdown_wrapper',
                                     'mock-data', 'tests.*']),
